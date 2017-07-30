2ff168a7ded53795cccd2cd883d67ec19a29fc82
Sergey Aldoukhov <saldoukhov@gmail.com>
2017-07-29 19:38:16 -0700

    Ported to shared code using SkiaSharp

---
+++ Cropper.Lib/Cropper.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using SkiaSharp;
+
+namespace Cropper.Lib
+{
+    internal class CropCalculator
+    {
+        internal static IObservable<SKRect> SelectRectangle(IObservable<CropHelper.MouseEvent> mouseEvents)
+        {
+            return Observable.Create<SKRect>(observer =>
+            {
+                var dropCoords = new SKRect(20, 20, 120, 120);
+                observer.OnNext(dropCoords);
+
+                var dragStarts = mouseEvents
+                    .Where(x => x.EventType == CropHelper.MouseEventType.Press)
+                    .Select(x => TestHitPoint(dropCoords, x.Coords))
+                    .Where(x => x != null);
+                var releases = mouseEvents
+                    .Where(x => x.EventType == CropHelper.MouseEventType.Release)
+                    .Select(x => x.Coords);
+                var moves = mouseEvents
+                    .Where(x => x.EventType == CropHelper.MouseEventType.Move)
+                    .Select(x => x.Coords);
+
+                var dropSub = dragStarts
+                    .SelectMany(x => releases
+                        .Take(1)
+                        .Select(y => MoveTarget(dropCoords, new Drag(x, y))))
+                    .Do(x => { dropCoords = x; })
+                    .Subscribe();
+
+                var dragSub = dragStarts
+                    .SelectMany(x => moves
+                        .TakeUntil(releases)
+                        .Select(y => MoveTarget(dropCoords, new Drag(x, y))))
+                    .DistinctUntilChanged()
+                    .Subscribe(observer);
+
+                return new CompositeDisposable(dragSub, dropSub);
+            });
+        }
+
+        static HitPoint TestHitPoint(SKRect target, SKPoint coords)
+        {
+            if (WithinCorner(new SKPoint(target.Left, target.Top), coords))
+                return new HitPoint(DragType.TopLeft, coords);
+            if (WithinCorner(new SKPoint(target.Left, target.Bottom), coords))
+                return new HitPoint(DragType.BottomLeft, coords);
+            if (WithinCorner(new SKPoint(target.Right, target.Top), coords))
+                return new HitPoint(DragType.TopRight, coords);
+            if (WithinCorner(new SKPoint(target.Right, target.Bottom), coords))
+                return new HitPoint(DragType.BottomRight, coords);
+            return target.Contains(coords)
+                ? new HitPoint(DragType.Drag, coords)
+                : null;
+        }
+
+        static bool WithinCorner(SKPoint target, SKPoint coords)
+        {
+            return Math.Abs(coords.X - target.X) < 10 && Math.Abs(coords.Y - target.Y) < 10;
+        }
+
+        enum DragType
+        {
+            Drag,
+            TopLeft,
+            BottomLeft,
+            TopRight,
+            BottomRight
+        }
+
+        class HitPoint
+        {
+            public DragType DragType { get; }
+            public SKPoint Coords { get; }
+
+            public HitPoint(DragType dragType, SKPoint coords)
+            {
+                DragType = dragType;
+                Coords = coords;
+            }
+        }
+
+        class Drag
+        {
+            public HitPoint Start { get; }
+            public SKPoint Delta { get; }
+
+            public Drag(HitPoint start, SKPoint end)
+            {
+                Start = start;
+                Delta = end - Start.Coords;
+            }
+        }
+
+        static SKRect MoveTarget(SKRect current, Drag drag)
+        {
+            float diag;
+            switch (drag.Start.DragType)
+            {
+                case DragType.Drag:
+                    return SKRect.Create(current.Location + drag.Delta, current.Size);
+                case DragType.TopLeft:
+                    diag = Math.Max(-drag.Delta.X, -drag.Delta.Y);
+                    return new SKRect(current.Left - diag, current.Top - diag, current.Right, current.Bottom);
+                case DragType.BottomLeft:
+                    diag = Math.Max(-drag.Delta.X, drag.Delta.Y);
+                    return new SKRect(current.Left - diag, current.Top, current.Right, current.Bottom + diag);
+                case DragType.TopRight:
+                    diag = Math.Max(drag.Delta.X, -drag.Delta.Y);
+                    return new SKRect(current.Left, current.Top - diag, current.Right + diag, current.Bottom);
+                case DragType.BottomRight:
+                    diag = Math.Max(drag.Delta.X, drag.Delta.Y);
+                    return new SKRect(current.Left, current.Top, current.Right + diag, current.Bottom + diag);
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    public static class CropHelper
+    {
+        public enum MouseEventType
+        {
+            Press,
+            Release,
+            Move
+        }
+
+        public class MouseEvent
+        {
+            public MouseEventType EventType { get; }
+            public SKPoint Coords { get; }
+
+            public MouseEvent(MouseEventType eventType, SKPoint coords)
+            {
+                EventType = eventType;
+                Coords = coords;
+            }
+        }
+
+        static void DrawCropArea(SKRect rect, SKCanvas canvas)
+        {
+            canvas.DrawColor(new SKColor(130, 130, 130));
+            canvas.DrawRect(rect, new SKPaint
+            {
+                Color = new SKColor(20, 20, 20)
+            });
+        }
+
+        public static IObservable<SKRect> CropImage(
+            IObservable<MouseEvent> mouseEvents,
+            Func<IObservable<SKCanvas>> canvasSelector)
+        {
+            return 
+                from rect in CropCalculator.SelectRectangle(mouseEvents)
+                from canvas in canvasSelector().Do(canvas => DrawCropArea(rect, canvas))
+                select rect;
+        }
+        
+    }
+}
--- Cropper/MainPage.xaml.cs
+++ Cropper/MainPage.xaml.cs
@@ -1,14 +1,18 @@
 ﻿using System;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using Windows.Foundation;
-using Windows.UI.Xaml.Controls;
+using System.Reactive.Subjects;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Input;
+using Cropper.Lib;
+using SkiaSharp;
+using SkiaSharp.Views.UWP;
 
 namespace Cropper
 {
     public sealed partial class MainPage
     {
+        readonly Subject<SKCanvas> whenNeedPaint = new Subject<SKCanvas>();
+
         public MainPage()
         {
             InitializeComponent();
@@ -16,160 +20,33 @@ namespace Cropper
                 .FromEventPattern<PointerEventHandler, PointerRoutedEventArgs>(
                     h => Container.PointerPressed += h,
                     h => Container.PointerPressed -= h)
-                .Select(x => new MouseEvent(MouseEventType.Press, x.EventArgs.GetCurrentPoint(Container).Position));
+                .Select(x => new CropHelper.MouseEvent(CropHelper.MouseEventType.Press, x.EventArgs.GetCurrentPoint(Container).Position.ToSKPoint()));
             var releases = Observable.FromEventPattern<PointerEventHandler, PointerRoutedEventArgs>(
                     h => Container.PointerReleased += h,
                     h => Container.PointerReleased -= h)
-                .Select(x => new MouseEvent(MouseEventType.Release, x.EventArgs.GetCurrentPoint(Container).Position));
+                .Select(x => new CropHelper.MouseEvent(CropHelper.MouseEventType.Release, x.EventArgs.GetCurrentPoint(Container).Position.ToSKPoint()));
             var moves = Observable.FromEventPattern<PointerEventHandler, PointerRoutedEventArgs>(
                     h => Container.PointerMoved += h,
                     h => Container.PointerMoved -= h)
-                .Select(x => new MouseEvent(MouseEventType.Move, x.EventArgs.GetCurrentPoint(Container).Position));
+                .Select(x => new CropHelper.MouseEvent(CropHelper.MouseEventType.Move, x.EventArgs.GetCurrentPoint(Container).Position.ToSKPoint()));
 
             var mouseEvents = Observable.Merge(presses, releases, moves);
-            SelectRectangle(mouseEvents).Subscribe(PositionTarget);
+            CropHelper
+                .CropImage(mouseEvents, () =>
+                {
+                    Container.Invalidate();
+                    return whenNeedPaint.Take(1);
+                })
+                .Subscribe();
         }
 
-        static IObservable<Rect> SelectRectangle(IObservable<MouseEvent> mouseEvents)
+        void Canvas_OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
-            return Observable.Create<Rect>(observer =>
-            {
-                var dropCoords = new Rect(20, 20, 120, 120);
-                observer.OnNext(dropCoords);
-
-                var dragStarts = mouseEvents
-                    .Where(x => x.EventType == MouseEventType.Press)
-                    .Select(x => TestHitPoint(dropCoords, x.Coords))
-                    .Where(x => x != null);
-                var releases = mouseEvents
-                    .Where(x => x.EventType == MouseEventType.Release)
-                    .Select(x => x.Coords);
-                var moves = mouseEvents
-                    .Where(x => x.EventType == MouseEventType.Move)
-                    .Select(x => x.Coords);
-
-                var dropSub = dragStarts
-                    .SelectMany(x => releases
-                        .Take(1)
-                        .Select(y => MoveTarget(dropCoords, new Drag(x, y))))
-                    .Do(x => { dropCoords = x; })
-                    .Subscribe();
-
-                var dragSub = dragStarts
-                    .SelectMany(x => moves
-                        .TakeUntil(releases)
-                        .Select(y => MoveTarget(dropCoords, new Drag(x, y))))
-                    .DistinctUntilChanged()
-                    .Subscribe(observer);
-
-                return new CompositeDisposable(dragSub, dropSub); 
-            });
+            whenNeedPaint.OnNext(e.Surface.Canvas);
         }
 
-        static Rect MoveTarget(Rect current, Drag delta)
+        void ButtonClear_OnClick(object sender, RoutedEventArgs e)
         {
-            double diag;
-            switch (delta.Start.DragType)
-            {
-                case DragType.Drag:
-                    return new Rect(current.X + delta.Width, current.Y + delta.Height, current.Width, current.Height);
-                case DragType.TopLeft:
-                    diag = Math.Max(-delta.Width, -delta.Height);
-                    return new Rect(current.X -diag, current.Y -diag, current.Width + diag, current.Height + diag);
-                case DragType.BottomLeft:
-                    diag = Math.Max(-delta.Width, delta.Height);
-                    return new Rect(current.X - diag, current.Y, current.Width + diag, current.Height + diag);
-                case DragType.TopRight:
-                    diag = Math.Max(delta.Width, -delta.Height);
-                    return new Rect(current.X, current.Y - diag, current.Width + diag, current.Height + diag);
-                case DragType.BottomRight:
-                    diag = Math.Max(delta.Width, delta.Height);
-                    return new Rect(current.X, current.Y, current.Width + diag, current.Height + diag);
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        static HitPoint TestHitPoint(Rect target, Point coords)
-        {
-            if (WithinCorner(new Point(target.Left, target.Top), coords))
-                return new HitPoint(DragType.TopLeft, coords);
-            if (WithinCorner(new Point(target.Left, target.Bottom), coords))
-                return new HitPoint(DragType.BottomLeft, coords);
-            if (WithinCorner(new Point(target.Right, target.Top), coords))
-                return new HitPoint(DragType.TopRight, coords);
-            if (WithinCorner(new Point(target.Right, target.Bottom), coords))
-                return new HitPoint(DragType.BottomRight, coords);
-            return target.Contains(coords) 
-                ? new HitPoint(DragType.Drag, coords) 
-                : null;
-        }
-
-        static bool WithinCorner(Point target, Point coords)
-        {
-            return Math.Abs(coords.X - target.X) < 10 && Math.Abs(coords.Y - target.Y) < 10;
-        }
-
-        public enum MouseEventType
-        {
-            Press,
-            Release,
-            Move
-        }
-
-        public enum DragType
-        {
-            Drag,
-            TopLeft,
-            BottomLeft,
-            TopRight,
-            BottomRight
-        }
-
-        public class HitPoint
-        {
-            public DragType DragType { get; }
-            public Point Coords { get; }
-
-            public HitPoint(DragType dragType, Point coords)
-            {
-                DragType = dragType;
-                Coords = coords;
-            }
-        }
-
-        public class MouseEvent
-        {
-            public MouseEventType EventType { get; }
-            public Point Coords { get; }
-
-            public MouseEvent(MouseEventType eventType, Point coords)
-            {
-                EventType = eventType;
-                Coords = coords;
-            }
-        }
-
-        public class Drag
-        {
-            public HitPoint Start { get; }
-            public Point End { get; }
-            public double Width => End.X - Start.Coords.X;
-            public double Height => End.Y - Start.Coords.Y;
-
-            public Drag(HitPoint start, Point end)
-            {
-                Start = start;
-                End = end;
-            }
-        }
-
-        void PositionTarget(Rect toRect)
-        {
-            Canvas.SetLeft(Target, toRect.X);
-            Canvas.SetTop(Target, toRect.Y);
-            Target.Width = toRect.Width;
-            Target.Height = toRect.Height;
         }
     }
 }
